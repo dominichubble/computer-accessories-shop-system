0abd9665a270ab12fe34c22a37871a9b18053f2c
Dominic Hubble <dominichubble@gmail.com>
2024-05-02 20:21:07 +0100

    Update GUI layout and stock information

--- src/customerFrame.java
+++ src/customerFrame.java
@@ -1,19 +1,27 @@
 
-
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
 public class CustomerFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTable tblProducts;
+	private DefaultTableModel dtmProducts;
+	private JTextField barcodeInput;
 
 	/**
 	 * Launch the application.
@@ -42,7 +50,7 @@ public class CustomerFrame extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JButton logout = new JButton("Logout");
 		logout.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -53,16 +61,81 @@ public class CustomerFrame extends JFrame {
 		});
 		logout.setBounds(894, 23, 89, 23);
 		contentPane.add(logout);
-		
+
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(10, 45, 988, 479);
 		contentPane.add(tabbedPane);
-		
-		JPanel panel = new JPanel();
-		tabbedPane.addTab("New tab", null, panel, null);
-		
+
+		dtmProducts = new DefaultTableModel();
+		dtmProducts.setColumnIdentifiers(new String[] { "Barcode", "Category", "Type", "Brand", "Colour",
+				"Connectivity", "Stock", "Price", "Info" });
+
+		JPanel panelShop = new JPanel();
+		tabbedPane.addTab("Shop", null, panelShop, null);
+		panelShop.setLayout(null);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 864, 229);
+		panelShop.add(scrollPane);
+
+		tblProducts = new JTable();
+		scrollPane.setViewportView(tblProducts);
+		tblProducts.setModel(dtmProducts);
+
+		JButton btnView = new JButton("View");
+		btnView.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				populateTable();
+			}
+		});
+		btnView.setBounds(884, 11, 89, 72);
+		panelShop.add(btnView);
+
+		JLabel txtAddProduct = new JLabel("Add Product");
+		txtAddProduct.setBounds(20, 251, 73, 14);
+		panelShop.add(txtAddProduct);
+
+		barcodeInput = new JTextField();
+		barcodeInput.setBounds(10, 276, 86, 20);
+		panelShop.add(barcodeInput);
+		barcodeInput.setColumns(10);
+
+		JLabel txtInsertBarcode = new JLabel("(insert barcode)");
+		txtInsertBarcode.setBounds(106, 279, 89, 14);
+		panelShop.add(txtInsertBarcode);
+
+		JButton btnAddToBasket = new JButton("Add To Basket");
+		btnAddToBasket.setBounds(10, 307, 107, 23);
+		panelShop.add(btnAddToBasket);
+
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("New tab", null, panel_1, null);
 	}
 
+	public void populateTable() {
+		List<Product> products = StockReader.readStockFile("data/Stock.txt");
+		DefaultTableModel model = (DefaultTableModel) tblProducts.getModel();
+		model.setRowCount(0); // Clear existing data
+
+		for (Product product : products) {
+
+			String deviceType = "";
+			String additionalInfo = "";
+
+			if (product instanceof Keyboard) {
+				Keyboard keyboard = (Keyboard) product;
+				deviceType = keyboard.getDeviceType().toString();
+				additionalInfo = keyboard.getAdditionalInfo().toString();
+			} else if (product instanceof Mouse) {
+				Mouse mouse = (Mouse) product;
+				deviceType = mouse.getDeviceType().toString();
+				additionalInfo = Integer.toString(mouse.getAdditionalInfo());
+			}
+
+			model.addRow(new Object[] { product.getBarcode(), product.getCategory(), deviceType, product.getBrand(),
+					product.getColor(), product.getConnectivity(), product.getQuantityInStock(),
+					product.getRetailPrice(), additionalInfo });
+		}
+
+	}
 }
